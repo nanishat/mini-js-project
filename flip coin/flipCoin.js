a763bb30ac33606f4b2fcf1cd6194acf85470214
nanishat <spy.spid3y@gmail.com>
2025-08-13 16:34:43 +0600

    some changes

--- flip coin/flipCoin.js
+++ flip coin/flipCoin.js
@@ -1,33 +1,53 @@
-document.querySelectorAll('.game-play').forEach(button => {
-    button.addEventListener('click', () => {
-        playGame(button.textContent);
-    });
-});
+const headsBtn = document.querySelector('.js-heads-btn');
+const tailsBtn = document.querySelector('.js-tails-btn');
+const displayMoves = document.querySelector('.js-moves');
+const displayResult = document.querySelector('.js-result');
+const displayScores = document.querySelector('.js-score');
+const clearBtn = document.querySelector('.js-clear-btn');
 
-document.getElementById('clear').addEventListener('click', () => {
-    score.wins = 0;
-    score.losses = 0;
-    localStorage.removeItem('score');
-    console.log('reset score: ', score);
-});
+//clear-button
+clearBtn.addEventListener('click', clearButtonFeature);
 
+//parse-&-assigning-values
 score = JSON.parse(localStorage.getItem('score')) || {
     wins: 0,
     losses: 0
 };
 
-function playGame(guess) {
-    const randomNumber = Math.random();
-    const result = randomNumber < 0.5 ? 'heads' : 'tails';
+//event-listener-handler
+headsBtn.addEventListener('click', () => {
+    playGame('heads');
+});
 
-    if (result === 'heads') {
-        score.wins += 1;
-    } else {
+tailsBtn.addEventListener('click', () => {
+    playGame('tails');
+});
+
+
+function playGame(playerPick) {
+    //computer-moves
+    const randomNumber = Math.random();
+    let computerPick = randomNumber < 0.5 ? 'heads' : 'tails';
+
+    //checks-logic
+    if (playerPick === computerPick) {
         score.losses += 1;
+    } else {
+        score.wins += 1;
     }
 
     localStorage.setItem('score', JSON.stringify(score));
 
-    console.log(guess === result ? 'You win!' : 'You lose!');
-    console.log(score);
+    displayMoves.innerHTML = `You ${playerPick} - ${computerPick} Computer`;
+    displayResult.innerHTML = (playerPick === computerPick ? 'You lose!' : 'You win!');
+    displayScores.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}`;
+}
+
+function clearButtonFeature() {
+    score.wins = 0;
+    score.losses = 0;
+    localStorage.removeItem('score');
+    displayMoves.innerHTML = '';
+    displayResult.innerHTML = `Your score has been reset`;
+    displayScores.innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}`;
 }
--- ~others/amazon shipping calculator/asc.js
+++ ~others/amazon shipping calculator/asc.js
@@ -3,14 +3,14 @@ const calculateElement = document.querySelector('.js-calculate-cost');
 const resultElement = document.querySelector('.js-total-cost');
 
 function calculateTotal() {
-    let cost = Number(inputElement.value);
+    let cost = Number(inputElement.value) * 100;
     if (isNaN(cost) || cost < 0) {
         resultElement.innerHTML = 'Please enter a valid positive number.';
         return;
-    } else if (cost < 40) {
-        cost += 10;
+    } else if (cost < 4000) {
+        cost += 1000;
     }
-    resultElement.innerHTML = `$${cost}`;
+    resultElement.innerHTML = `$${cost / 100}`;
 }
 
 inputElement.addEventListener('keydown', (event) => {
