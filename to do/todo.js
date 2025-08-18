53c95ef95c46ef3e5c5c148eec0c4aab2689f175
nanishat <spy.spid3y@gmail.com>
2025-08-18 12:11:17 +0600

    phase 2, generated html to render display

--- to do/todo.js
+++ to do/todo.js
@@ -1,4 +1,4 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
 
 document.querySelector('.js-add-btn')
     .addEventListener('click', () => {
@@ -12,4 +12,19 @@ function addTodo() {
     todoList.push(name); //push to the array
     console.log(todoList); //console output of the array
     inputElement.value = ''; //this reset the input box
+    renderTodoList();
+}
+
+
+function renderTodoList() {
+    let todoListHTML = ''; //an empty string to display HTML through JS
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todos = todoList[i];
+        const html = `<p>${todos}</p>`; //generated HTML 
+        todoListHTML += html;
+    }
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
 }
